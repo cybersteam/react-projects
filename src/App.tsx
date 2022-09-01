897ca025e5c49fee20ef18ce5795e517f1dfe433
ivan <infinite22@gmail.com>
2022-09-01 21:41:11 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import './Styles/App.css'
+
+const Square = (props: any) => {
+  
+  return(
+    <button 
+      className="square"
+      onClick={props.onClickEvent}
+      >
+      {props.value}
+    </button>
+  );
+};
+
+const Board = () => {
+  /* const initialSquares = [
+    null,null,null,
+    null,null,null,
+    null,null,null,
+  ]; */
+  const initialSquares = Array(9).fill(null);
+  const [squares, setSquares] = useState(initialSquares);
+  //initial state of xIsnext set to true (first player plays X)
+  const [xIsNext, setXisNext] = useState(true);
+
+  const handleClickEvent = (i: any) => {
+    //make a copy of the squares state array
+    const newSquares = [...squares];
+    // mutate the copy setting it's i element to xIsNext boolean value
+    newSquares[i] = xIsNext ? "0" : "1";
+    //call the setsquares function with the mutated copy
+    setSquares(newSquares);
+    setXisNext(!xIsNext);
+  };
+  const renderSquare = (i: any) => {
+    return(
+      
+        <Square 
+        value={squares[i]} 
+        onClickEvent={() => handleClickEvent(i)}
+        />
+      
+    )
+
+  }
+
+  const  status = `Next Player: ${xIsNext ?  '0' : '1'}`;
+
+  return (
+    <div className="board">
+      <h2>zero's n one's game</h2>
+      <div className="status">{status}</div>
+      <div className="boardrow">
+      {renderSquare(1)}{renderSquare(2)}{renderSquare(3)}
+      </div>
+      <div className="boardrow">
+        {renderSquare(4)}{renderSquare(5)}{renderSquare(6)}
+      </div>
+      <div className="boardrow">
+        {renderSquare(7)}{renderSquare(8)}{renderSquare(9)}
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return(
+  <div className="app">
+    
+    <Board />
+  </div>
+  );
+};
+  
+
+export default App
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,19 @@
+import "../Styles/NavBar.css"
+
+export default function NavBar(){
+    return(
+        <div className="navbar">
+            <div className="links">
+                <a href="/home">Home</a>
+                <a href="/about"> About</a>
+                <a href="/contact">Contact</a>
+                <a href="/faq">FAQ</a>
+                <a href="/fun">Fun</a>
+            </div>
+            
+
+        </div>
+    )
+
+
+}
